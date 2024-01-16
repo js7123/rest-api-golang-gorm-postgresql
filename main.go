07decd26674802c095e04906df3c218eb47c58ae
jsrepo742 <jsrepo742@gmail.com>
2024-01-16 00:42:33 -0800

    Updates github path for /initializers in main.go.

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/js7123/golang-gorm-postgres/initializers"
+	"github.com/js7123/rest-api-golang-gorm-postgres/initializers"
 )
 
 var (
